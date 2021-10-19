ea493c3fdafef99f58c8bb7292042a1764d0d122
CatalinaChirtes <adela.chirtes@gmail.com>
2021-10-19 20:50:25 +0300

    DevAcademy

---
+++ Context/BookManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using BookManagerAPI.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagerAPI.Context
+{
+    public class BookManagerContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=BookManager;Trusted_Connection=True");
+        }
+    }
+}
